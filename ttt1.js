9cd09e9771d4084bc0252620e9f2cad70e52a9cb
SaifL <leach_saif@yahoo.com>
2021-11-04 12:07:53 -0400

    First commit

---
+++ ttt1.js
@@ -0,0 +1,38 @@
+var p1score=0
+var p2score=0
+var tie=0
+var turn=1
+
+function checkwinner(){
+   if ($(".row-1 .box.X").length === 3 
+   || $(".row-2 .box.X").length === 3 
+   || $(".row-3 .box.X").length === 3
+   || $(".col-1.X").length === 3 
+   || $(".col-2.X").length === 3
+   || $(".col-3.X").length === 3
+   || $("#1").hasClass("X") 
+   &&$("#5").hasClass("X") 
+   &&$("#9").hasClass("X")
+   || $("#3").hasClass("X")
+   &&$("#5").hasClass("X")
+   &&$("#7").hasClass("X"))
+   {
+       console.log("Working")
+   }
+}
+
+$(".box").click(function(){
+    if(turn===1){
+        $(this).text("X")
+        $(this).addClass("X")
+        turn=2;
+        $("#turn").text(2)
+        checkwinner();
+    }
+    else{
+        $(this).text("O")
+        $(this).addClass("O")
+        turn=1
+        $("#turn").text(1)
+    }
+})
